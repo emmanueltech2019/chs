2d2afb9568b0a6ccc20eff0dffb448e7f20c7057
EMMANUEL LUCKY <emmanueltech2019@gmail.com>
2024-06-22 15:52:40 +0100

    first commit

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -9,11 +9,10 @@ interface Data {
 const data:Data ={
     brand:"CHS",
     city:"Charlotte",
-    state:"TX",
+    state:"NC",
     domain: "charlotteherniaspecialists.com",
     logo: 'https://res.cloudinary.com/wise-solution-inc/image/upload/v1719067604/3.3Asset_94_kjz0gp.png',
     src:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d417296.4487928816!2d-81.1694864802771!3d35.20326790782849!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88541fc4fc381a81%3A0x884650e6bf43d164!2sCharlotte%2C%20NC%2C%20USA!5e0!3m2!1sen!2sng!4v1719067677423!5m2!1sen!2sng"
-
 }
 
 export default data
